af10f821ebac41755373458c3cfc807e26e9ff45
Mario Gómez <mralejandrogu@gmail.com>
2021-06-14 14:02:39 -0400

    BASE DE DATOOOOS!
    Se ha realizado la conexión a la base de datos. Panel admin funcionando. Creada clase Mensaje con todos los campos correspondientes.

--- thetop100/web/models.py
+++ thetop100/web/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Mensaje(models.Model):
+   nomApellido = models.CharField(max_length=50, verbose_name="Nombre y Apellido")
+   email       = models.CharField(max_length=50, verbose_name="Email")
+   telefono    = models.CharField(max_length=12, verbose_name="Telefono")
+   asunto      = models.CharField(max_length=50, verbose_name="Asunto")
+   mensaje     = models.CharField(max_length=500, verbose_name="Mensaje")
